800a884a6d0e8aea1eea49dc8e1ff48968e1a609
megamisyn@gmail.com <megamisyn@gmail.com>
2020-09-15 16:08:48 -0300

    DTO

--- src/main/java/jp/syned/workshopmongo/domain/User.java
+++ src/main/java/jp/syned/workshopmongo/domain/User.java
@@ -3,6 +3,7 @@ package jp.syned.workshopmongo.domain;
 import java.io.Serializable;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
@@ -12,6 +13,7 @@ public class User implements Serializable{
 	@Id
 	private String id;
 	private String name;
+	@Indexed(unique=true)
 	private String email;
 	
 	public User() {
--- src/main/java/jp/syned/workshopmongo/resources/UserResource.java
+++ src/main/java/jp/syned/workshopmongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package jp.syned.workshopmongo.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import jp.syned.workshopmongo.domain.User;
+import jp.syned.workshopmongo.dto.UserDTO;
 import jp.syned.workshopmongo.services.UserService;
 
 @RestController
@@ -20,8 +22,9 @@ public class UserResource {
 	
 	//@RequestMapping(method=RequestMethod.GET)
 	@GetMapping
-	public ResponseEntity<List<User>> findAll(){
+	public ResponseEntity<List<UserDTO>> findAll(){
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 	}
 }

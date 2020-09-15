e6428a94f0dc8a80653c357ea75dd97d05c26168
megamisyn@gmail.com <megamisyn@gmail.com>
2020-09-15 19:35:25 -0300

    Consulta simples com query methods

--- src/main/java/jp/syned/workshopmongo/repository/PostRepository.java
+++ src/main/java/jp/syned/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package jp.syned.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import jp.syned.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
-	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }

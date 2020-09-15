5a2ce05b2e6906b5532212eab4730ef490e8c0b9
megamisyn@gmail.com <megamisyn@gmail.com>
2020-09-15 17:52:36 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/jp/syned/workshopmongo/config/Init.java
+++ src/main/java/jp/syned/workshopmongo/config/Init.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 
 import jp.syned.workshopmongo.domain.Post;
 import jp.syned.workshopmongo.domain.User;
+import jp.syned.workshopmongo.dto.AuthorDTO;
 import jp.syned.workshopmongo.repository.PostRepository;
 import jp.syned.workshopmongo.repository.UserRepository;
 
@@ -35,10 +36,11 @@ public class Init implements CommandLineRunner{
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null,sdf.parse("21/03/2018"),"Partiu Viagem","Vou viajar para Sao Paulo. Abracos!",maria);
-		Post post2 = new Post(null,sdf.parse("23/03/2018"),"Bom dia","Acordei feliz hoje",maria);
-		
 		userRepository.saveAll(Arrays.asList(maria,alex,bob));
+		
+		Post post1 = new Post(null,sdf.parse("21/03/2018"),"Partiu Viagem","Vou viajar para Sao Paulo. Abracos!",new AuthorDTO(maria));
+		Post post2 = new Post(null,sdf.parse("23/03/2018"),"Bom dia","Acordei feliz hoje",new AuthorDTO(maria));		
+		
 		postRepository.saveAll(Arrays.asList(post1,post2));		
 	}	
 }

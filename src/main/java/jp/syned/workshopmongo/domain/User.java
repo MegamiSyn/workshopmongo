818ca0f4788ef9480f17bbb8a3110745e7d9823c
megamisyn@gmail.com <megamisyn@gmail.com>
2020-09-15 19:05:00 -0300

    Obtendo um post por id

--- src/main/java/jp/syned/workshopmongo/domain/User.java
+++ src/main/java/jp/syned/workshopmongo/domain/User.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -15,6 +16,7 @@ public class User implements Serializable{
 	@Id
 	private String id;
 	private String name;
+	@Indexed(unique = true)
 	private String email;
 	@DBRef(lazy = true)
 	private List<Post> posts = new ArrayList<>();
---
+++ src/main/java/jp/syned/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package jp.syned.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import jp.syned.workshopmongo.domain.Post;
+import jp.syned.workshopmongo.repository.PostRepository;
+import jp.syned.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+		
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}		
+}
